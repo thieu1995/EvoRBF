f07aa10ea71e5868a6173f3a8907dd537fd07985
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-09-22 17:05:23 +0700

    Add MhaRbfRegressor class

---
+++ evorbf/model/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 17:03, 22/09/2023 ----------%
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
