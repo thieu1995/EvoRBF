b115372686acb9fe16ea50f4c10acf60d36fff17
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-09-22 16:56:07 +0700

    Add base_rbf module that hold numpy-based Class for RBF network

---
+++ evorbf/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 15:23, 10/08/2023 ----------%
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+__version__ = "0.0.1"
+
+from evorbf.utils.scaler import DataTransformer
+from evorbf.utils.data_loader import Data, get_dataset
