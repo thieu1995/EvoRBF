cd95ed95a370d259687f8fa272c55e30cb737df7
Thieu Nguyen <nguyenthieu2102@gmail.com>
2024-05-08 07:49:37 +0700

    Add init files

---
+++ examples/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 07:49, 08/05/2024 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
