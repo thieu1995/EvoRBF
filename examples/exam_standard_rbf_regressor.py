aa8d09dba55c75b9f4f1cee54bdf84f6a1abdfdc
Thieu Nguyen <nguyenthieu2102@gmail.com>
2024-05-10 22:27:51 +0700

    Add example for RbfRegressor

---
+++ examples/exam_standard_rbf_regressor.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 22:15, 10/05/2024 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+from evorbf import Data, RbfRegressor
+from sklearn.datasets import load_diabetes
+
+
+## Load data object
+# total samples = 442, total features = 10
+X, y = load_diabetes(return_X_y=True)
+data = Data(X, y)
+
+## Split train and test
+data.split_train_test(test_size=0.2, random_state=2, inplace=True)
+print(data.X_train.shape, data.X_test.shape)
+
+## Scaling dataset
+data.X_train, scaler_X = data.scale(data.X_train, scaling_methods=("standard", "minmax"))
+data.X_test = scaler_X.transform(data.X_test)
+
+data.y_train, scaler_y = data.scale(data.y_train, scaling_methods=("standard", "minmax"))
+data.y_test = scaler_y.transform(data.y_test)
+
+print(type(data.X_train), type(data.y_train))
+
+## Create model
+model = RbfRegressor(size_hidden=10, center_finder="kmean", sigmas=2.0, regularization=False, lamda=0.01, seed=42)
+
+## Train the model
+model.fit(data.X_train, data.y_train)
+
+## Test the model
+y_pred = model.predict(data.X_test)
+print(y_pred)
+
+## Calculate some metrics
+print(model.score(X=data.X_test, y=data.y_test, method="RMSE"))
+print(model.scores(X=data.X_test, y=data.y_test, list_methods=["R2", "NSE", "MAPE"]))
+print(model.evaluate(y_true=data.y_test, y_pred=y_pred, list_metrics=["R2", "NSE", "MAPE", "NNSE"]))
