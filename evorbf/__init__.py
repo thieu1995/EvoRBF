de0c0b6007f30f7e2aca8a7cc1fc289344c1293e
anh9895 <honganh090895@gmail.com>
2024-11-30 15:32:06 +0700

    Update __init__ file

--- evorbf/__init__.py
+++ evorbf/__init__.py
@@ -8,6 +8,7 @@ __version__ = "1.0.0"
 
 from evorbf.helpers.scaler import DataTransformer
 from evorbf.helpers.preprocessor import Data
-from evorbf.core.ina_rbf import InaRbfRegressor, InaRbfClassifier
+from evorbf.core.nia_rbf import NiaRbfRegressor, NiaRbfClassifier
 from evorbf.core.standard_rbf import RbfRegressor, RbfClassifier
 from evorbf.core.rbf_tuner import InaRbfTuner
+from evorbf.core.advanced_rbf import AdvancedRbfNet, AdvancedRbfRegressor, AdvancedRbfClassifier
